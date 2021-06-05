348b3c9fd66b401401dce761b12103d6a049e891
Eric Parsons <ericparsons@live.com>
2021-06-05 00:28:15 -0700

    first commit. just the basics.

---
+++ alpaca_wrappers/wrapper.hpp
@@ -0,0 +1,14 @@
+
+
+
+class wrapper {
+
+public:
+
+
+
+protected:
+
+private:
+
+}
---
+++ money.cpp
@@ -0,0 +1,14 @@
+/*
+using this to test wrappers and stuff. feel free to add a bunch of crap here
+*/
+
+#include
+
+int main(){
+
+
+
+
+
+    return 0;
+}
