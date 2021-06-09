de6460701645e14a292430649c473e019f44737d
Antwan Gaggi <tunahelmet@users.noreply.github.com>
2021-06-08 21:45:22 -0700

    Add RapidJson dep

--- src/money.cpp
+++ src/money.cpp
@@ -4,7 +4,17 @@ using this to test wrappers and stuff. feel free to add a bunch of crap here
 
 #include "money_bot/money_bot.h"
 
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+
 int foo()
 {
-    return 2;
+    const char* json = "{\"test\":2}";
+    rapidjson::Document d;
+    d.Parse(json);
+
+    // 2. Modify it by DOM.
+    rapidjson::Value& s = d["test"];
+    return s.GetInt();
 }
